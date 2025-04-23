3434a3cc600170df1583106f523701b4b7d9612f
Mubarak Sani <sani.nooridev@gmail.com>
2025-04-23 16:35:07 +0530

    initial next js project setup

---
+++ app/page.js
@@ -0,0 +1,23 @@
+
+"use client";
+
+import React from 'react';
+
+export default function Home() {
+  return (
+    <main >
+      <style jsx>{`
+        main {
+          text-align: center;
+          padding: 2rem;
+        }
+        p {
+        margin-top: 1rem;
+        font-size: 1.3rem;
+        }
+      `}</style>
+      <h1>Welcome to Next.js!</h1>
+      <p>This is a simple Next.js application. deployed and configured by <strong>Vercel</strong> environment</p>
+    </main>
+  );
+}
