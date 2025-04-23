7947034ee83cc36e98cf4a7636414d700c980d74
Mubarak Sani <sani.nooridev@gmail.com>
2025-04-23 17:01:03 +0530

    Refactor CSS and update Next.js configuration for gh-pages deployment

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 "use client";
 
 import React from 'react';
+import "./globals.css"; 
 
 export default function Home() {
   return (
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
+    output: 'export',
+    basePath: '/next-gh-pages-app',
 };
 
 export default nextConfig;
